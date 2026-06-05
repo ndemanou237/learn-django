f271856589de987576dd0d1e47ef0072c10ceb12
ndemanou237 <ndemanoudonald9@gmail.com>
2026-06-05 19:24:56 +0100

    form

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ContactMessage
+from .forms import ContactForm
 
 def home_page_view(request):
     context = {
@@ -13,7 +14,20 @@ def home_page_view(request):
 
 
 def contact_page_view(request):
-    return render(request, 'contact.html')
+    success_msg = None
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            success_msg = "votre message a été bien envoyé !"
+            form = ContactForm()
+    else:
+        form = ContactForm()
+    context = {
+        'form':form,
+        'success_msg':success_msg
+    }    
+    return render(request, 'contact.html',context)
 
 def propos_page_view(request):
     return render(request,'propos.html')
