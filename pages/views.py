78cfb4055123767f9454a88c07944490decec54e
ndemanou237 <ndemanoudonald9@gmail.com>
2026-06-06 22:18:30 +0100

    inscription

--- pages/views.py
+++ pages/views.py
@@ -6,6 +6,7 @@ from django.views.generic import TemplateView
 from django.views.generic import ListView
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 class HomePageView(TemplateView):
     template_name = "home.html"
@@ -16,6 +17,7 @@ class HomePageView(TemplateView):
     
 class SignupView(CreateView):
     form_class = UserCreationForm
+    success_url = reverse_lazy("login")
     template_name = "registration/signup.html"    
 
 # def home_page_view(request):
